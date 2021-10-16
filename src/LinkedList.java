c1f89853c0eb1c3412c7852d45e86575ce5ca829
Mohammad rahimi <moyi.pary@gmail.com>
2021-10-17 02:36:58 +0330

    first initialazing of linked LinkedList
    add first method: "addLast" in the LinkedList class

---
+++ src/LinkedList.java
@@ -0,0 +1,25 @@
+public class LinkedList {
+
+    private class Node {
+
+        private int value;
+        private Node next;
+
+        private Node(int item) {
+            value = item;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+        if (first == null)
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var list = new LinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+    }
+}
