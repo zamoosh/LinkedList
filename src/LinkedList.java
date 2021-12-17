42aa62913f97a1b2f6c96faf07fcf98fad00d2a4
Mohammad Rahimi <moyi.pary@gmail.com>
2021-12-18 00:30:10 +0330

    Add generic types in project

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,41 +1,68 @@
-public class LinkedList {
+import java.util.Arrays;
 
+public class LinkedList<T> {
     private class Node {
 
-        private int value;
+        private T value;
         private Node next;
 
-        private Node(int item) {
+        private Node(T item) {
             value = item;
         }
     }
 
-    private Node first;
-    private Node last;
+    private Node tail;
+    private Node head;
+    private Integer size = 0;
 
-    public void addLast(int item) {
-        var node = new Node(item);
-        if (first == null)
-            first = last = node;
-        else {
-            last.next = node;
-            last = node;
+    public void add(T item, int index) {
+        this.addLast(null);
+        if (index < 0 || index > size - 1)
+            throw new ArrayIndexOutOfBoundsException();
+        if (index == 0) {
+            this.addFirst(item);
+            return;
         }
+        if (index == this.size - 1) {
+            this.addLast(item);
+            return;
+        }
+        var previous = this.getPrevious(head);
+        var current = this.head;
+        for (int i = size - 1; i > index; i--) {
+            current.value = previous.value;
+            previous = this.getPrevious(previous);
+            current = this.getPrevious(current);
+        }
+        previous.next.value = item;
+        this.size += 1;
     }
 
-    public void addFirst(int item) {
+    public void addLast(T item) {
         var node = new Node(item);
-        if (first == null)
-            first = last = node;
+        if (tail == null)
+            tail = head = node;
         else {
-            node.next = first;
-            first = node;
+            head.next = node;
+            head = node;
         }
+        this.size += 1;
     }
 
-    public int indexOf(int item) {
+    public void addFirst(T item) {
+        var node = new Node(item);
+        if (tail == null)
+            tail = head = node;
+        else {
+            node.next = tail;
+            tail = node;
+        }
+        this.size += 1;
+    }
+
+    public int indexOf(T item) {
         int index = 0;
-        var current = this.first;
+        var current = this.tail;
         while (current != null) {
             if (current.value == item) return index;
             current = current.next;
@@ -44,8 +71,8 @@ public class LinkedList {
         return -1;
     }
 
-    public Boolean contains(int item) {
-        var current = first;
+    public Boolean contains(T item, String... strings) {
+        var current = tail;
         while (current != null) {
             if (current.value == item) return true;
             current = current.next;
@@ -54,22 +81,44 @@ public class LinkedList {
     }
 
     public void removeFirst() {
-        first = first.next;
+        tail = tail.next;
+        this.size -= 1;
     }
 
     public void removeLast() {
-        last = getPrevious(last);
-        if (last != null) last.next = null;
-
-
+        head = getPrevious(head);
+        if (head != null) head.next = null;
+        this.size -= 1;
     }
 
     private Node getPrevious(Node node) {
-        var current = first;
+        var current = tail;
         while (current != null) {
             if (current.next == node) return current;
             current = current.next;
         }
         return null;
+    }
+
+    public Integer size() {
+        return this.size;
+    }
+
+    public T[] toArray() {
+        T[] arr = (T[]) new Object[this.size];
+        int i = 0;
+        var current = tail;
+        while (current != null) {
+            arr[i] = current.value;
+            if (current.next == null) return arr;
+            current = current.next;
+            i += 1;
+        }
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(this.toArray());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        var list = new LinkedList();
-        list.addLast(10);    //0
-        list.addLast(20);    //1
-        list.addLast(30);    //2
-        list.removeLast();
-//        list.addFirst(11);
-//        list.addFirst(22);
-//        list.addFirst(33);
+        var inputList = new LinkedList<String>();
+        inputList.addLast("mohammad");
+        inputList.addLast("ali");
+        inputList.addLast("sina");
+        inputList.addFirst("mahdi");
+        inputList.addFirst("javad");
+        System.out.println(inputList);
     }
 }
